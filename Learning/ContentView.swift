46ae5c22638035d22572dfcc6d48977518ef49e7
Semih Cetin <semihcettin@gmail.com>
2022-09-21 21:54:51 +0300

    Initial Commit

---
+++ Learning/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Learning
+//
+//  Created by Semih Cetin on 21.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Semih Cetin on 21.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
